4db914f3de4bb67e46e60019da435e880bd0a73c
Pranshu Jain <pranshuj012@gmail.com>
2021-12-15 16:23:44 +0530

    Grunt Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,11 @@
-// $(document).ready(function(){
-//     $('[data-toggle="tooltip"]').tooltip();
-// });
-
-// $(document).ready(function(){
-//     $("#mycarousel").carousel( { interval: 2000 } );
-
-//     $("#carouselButton").click(function(){
-//         if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-//             $("#mycarousel").carousel('pause');
-//             $("#carouselButton").children("span").removeClass('fa-pause');
-//             $("#carouselButton").children("span").addClass('fa-play');
-//         }
-//         else if ($("#carouselButton").children("span").hasClass('fa-play')){
-//             $("#mycarousel").carousel('cycle');
-//             $("#carouselButton").children("span").removeClass('fa-play');
-//             $("#carouselButton").children("span").addClass('fa-pause');                    
-//         }
-//     });
-    
-//     $("#loginButton").click(function(){
-//         $("#loginModal").modal('show');
-//     });
-
-//     $(".closeLogin").click(function(){
-//         $("#loginModal").modal('hide');
-//     });
-
-//     $("#openReserve").click(function(){
-//         $("#reservetable").modal('show');
-//     });
-
-//     $(".closeReserve").click(function(){
-//         $("#reservetable").modal('hide');
-//     });
-    
-// });
-
-$(function(){
+$(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-$(function(){
+$(document).ready(function(){
     $("#mycarousel").carousel( { interval: 2000 } );
 
-    $("#carouselButton").trigger(function(){
+    $("#carouselButton").click(function(){
         if ($("#carouselButton").children("span").hasClass('fa-pause')) {
             $("#mycarousel").carousel('pause');
             $("#carouselButton").children("span").removeClass('fa-pause');
@@ -56,20 +18,58 @@ $(function(){
         }
     });
     
-    $("#loginButton").trigger(function(){
+    $("#loginButton").click(function(){
         $("#loginModal").modal('show');
     });
 
-    $(".closeLogin").trigger(function(){
+    $(".closeLogin").click(function(){
         $("#loginModal").modal('hide');
     });
 
-    $("#openReserve").trigger(function(){
+    $("#openReserve").click(function(){
         $("#reservetable").modal('show');
     });
 
-    $(".closeReserve").trigger(function(){
+    $(".closeReserve").click(function(){
         $("#reservetable").modal('hide');
     });
     
 });
+
+// $(function(){
+//     $('[data-toggle="tooltip"]').tooltip();
+// });
+
+// $(function(){
+//     $("#mycarousel").carousel( { interval: 2000 } );
+
+//     $("#carouselButton").trigger(function(){
+//         if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+//             $("#mycarousel").carousel('pause');
+//             $("#carouselButton").children("span").removeClass('fa-pause');
+//             $("#carouselButton").children("span").addClass('fa-play');
+//         }
+//         else if ($("#carouselButton").children("span").hasClass('fa-play')){
+//             $("#mycarousel").carousel('cycle');
+//             $("#carouselButton").children("span").removeClass('fa-play');
+//             $("#carouselButton").children("span").addClass('fa-pause');                    
+//         }
+//     });
+    
+//     $("#loginButton").trigger(function(){
+//         $("#loginModal").modal('show');
+//     });
+
+//     $(".closeLogin").trigger(function(){
+//         $("#loginModal").modal('hide');
+//     });
+
+//     $("#openReserve").trigger(function(){
+//         $("#reservetable").modal('show');
+//     });
+
+//     $(".closeReserve").trigger(function(){
+//         $("#reservetable").modal('hide');
+//     });
+    
+// });
